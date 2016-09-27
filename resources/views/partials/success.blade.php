3717ccfae9c3bb4ccc7078c3dd4519508c4aafdd
Rushal <TheRushal@gmail.com>
2016-09-26 22:48:00 -0500

    Alert styling

--- resources/views/partials/success.blade.php
+++ resources/views/partials/success.blade.php
@@ -1,9 +1,11 @@
 @if (Session::has('success'))
     <div class="alert alert-success">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>
-            <i class="fa fa-check-circle fa-lg fa-fw"></i> Success.
-        </strong>
-        {{ Session::get('success') }}
+        <button type="button" class="btn-floating btn-flat" data-dismiss="alert"><i class="material-icons">&#xE5CD;</i></button>
+
+        <span><i class="material-icons">&#xE5CA;</i> {{ Session::get('success') }}</span>
     </div>
+
+    <script>
+        Materialize.toast({{ Session::get('success') }}, 4000)
+    </script>
 @endif
