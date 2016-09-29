6de6db6aedd394c9cabb7782b309bfb256597054
Rushal <TheRushal@gmail.com>
2016-09-28 22:08:55 -0500

    Some dashboard changes

--- resources/views/dashboard/partials/nav-partial.blade.php
+++ resources/views/dashboard/partials/nav-partial.blade.php
@@ -1,4 +1,4 @@
-<div class="navbar-fixed">
+<div class="navbar-fixed hide-on-large-only">
     <nav>
         <div class="nav-wrapper">
             <a href="#" data-activates="side-nav" class="button-collapse right"><i class="material-icons">menu</i></a>
@@ -7,25 +7,17 @@
             </a>
 
             <div class="row">
-                <!--<ul id="nav-mobile" class="hide-on-med-and-down col offset-s4">
-                    <li><a href="{{ url('/') }}"{!! \Request::is('/') ? ' class="active"' : '' !!}>@lang('navigation.home')</a></li>
-                    <li><a href="{{ url('/how-it-works') }}"{!! \Request::is('how-it-works') ? ' class="active"' : '' !!}>@lang('navigation.howitworks')</a></li>
-                    <li><a href="{{ url('/history') }}"{!! \Request::is('history') ? ' class="active"' : '' !!}>@lang('navigation.history')</a></li>
-                    <li><a href="{{ url('/bookkeeping') }}"{!! \Request::is('bookkeeping') ? ' class="active"' : '' !!}>@lang('navigation.bookkeeping')</a></li>
-                    <li><a href="{{ url('/contact') }}"{!! \Request::is('contact') ? ' class="active"' : '' !!}>@lang('navigation.contact')</a></li>
-                </ul>-->
-
                 <ul class="right nav-login">
-                    @if (Auth::guest())
-                        <li>
-                            @if (!\Agent::isMobile())
-                                <a href="#login-modal" class="modal-trigger">@lang('navigation.login')</a>
-                            @else
-                                <a href="{{ url('/login') }}"{!! \Request::is('login') ? ' class="active"' : '' !!}>@lang('navigation.login')</a>
-                            @endif
-                        </li>
-                    @else
-                        <li><a class="dropdown-button" href="#!" data-activates="account-dropdown" data-beloworigin="true"><i class="material-icons right">account_circle</i></a></li>
+                    <li><a href="{{ url('/dashboard') }}" class="hide-on-small-only inline waves-effect tooltipped" data-tooltip="@lang('dashboard.dashboard')"><i class="material-icons">&#xE871;</i></a></li>
+                    <li><a href="#" class="hide-on-small-only inline waves-effect tooltipped" data-tooltip="@lang('dashboard.account-summary')"><i class="material-icons">&#xE8A6;</i></a></li>
+                    <li>
+                        <a href="{{ url('/logout') }}" class="tooltipped" data-tooltip="@lang('navigation.logout')" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="material-icons">&#xE879;</i></a>
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+
+                        <!--<li><a class="dropdown-button" href="#!" data-activates="account-dropdown" data-beloworigin="true"><i class="material-icons right">account_circle</i></a></li>
                         <ul id="account-dropdown" class="dropdown-content">
                             <div class="caret"></div>
                             <li><a href="/dashboard">@lang('navigation.dashboard')</a></li>
@@ -36,8 +28,7 @@
                                     {{ csrf_field() }}
                                 </form>
                             </li>
-                        </ul>
-                    @endif
+                        </ul>-->
                 </ul>
             </div>
         </div>
@@ -48,8 +39,15 @@
     <li><a href="/" class="logo"><img src="{{ asset('images/logo.png') }}" width="80%" alt="@lang('navigation.logoalt')" style="margin-top: 20px;" /></a></li>
     <li><div class="divider"></div></li>
 
-    <li><a href="{{ url('/dashboard') }}" class="waves-effect{!! \Request::is('/') ? ' active' : '' !!}"><i class="material-icons">&#xE871;</i> @lang('dashboard.dashboard')</a></li>
-    <li><a href="{{ url('#') }}" class="waves-effect{!! \Request::is('how-it-works') ? ' active' : '' !!}"><i class="material-icons">&#xE851;</i> @lang('dashboard.account-summary')</a></li>
+    <li class="side-nav-inline">
+        <a href="{{ url('/dashboard') }}" class="inline waves-effect tooltipped" data-tooltip="@lang('dashboard.dashboard')"><i class="material-icons">&#xE871;</i></a>
+        <a href="#" class="inline waves-effect tooltipped" data-tooltip="@lang('dashboard.account-summary')"><i class="material-icons">&#xE8A6;</i></a>
+        <a href="{{ url('logout') }}" class="inline waves-effect tooltipped" data-tooltip="@lang('navigation.logout')" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <i class="material-icons">&#xE879;</i>
+        </a>
+    </li>
+    <li><div class="divider"></div></li>
+
     <li><a href="{{ url('#') }}" class="waves-effect{!! \Request::is('history') ? ' active' : '' !!}"><i class="material-icons">&#xE84F;</i> @lang('dashboard.billing')</a></li>
     <li><a href="{{ url('#') }}" class="waves-effect{!! \Request::is('bookkeeping') ? ' active' : '' !!}"><i class="material-icons">&#xE0AF;</i> @lang('dashboard.organization')</a></li>
     <li><a href="{{ url('#') }}" class="waves-effect{!! \Request::is('contact') ? ' active' : '' !!}"><i class="material-icons">&#xE8D3;</i> @lang('dashboard.user-management')</a></li>
