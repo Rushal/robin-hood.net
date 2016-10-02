9bfdb026d92ee653c3e4540bcaaa6a189cf4819d
Rushal <TheRushal@gmail.com>
2016-10-01 23:15:57 -0500

    added portrait 768 bg

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,30 +4,30 @@
 <div class="forest">
     <div class="tutorial"></div>
     <div class="planks">
-        <div class="plank plank-1"><img src="images/ad_logos/fedex.png" class="traveler-logo"/></div>
-        <div class="plank plank-2"><img src="images/ad_logos/citi.png" class="traveler-logo"/></div>
-        <div class="plank plank-3"><img src="images/ad_logos/dell.png" class="traveler-logo"/></div>
-        <div class="plank plank-4"><img src="images/ad_logos/cheveron.png" class="traveler-logo"/></div>
-        <div class="plank plank-5"><img src="images/ad_logos/citi.png" class="traveler-logo"/></div>
-        <div class="plank plank-6"><img src="images/ad_logos/dell.png" class="traveler-logo"/></div>
-        <div class="plank plank-7"><img src="images/ad_logos/fedex.png" class="traveler-logo"/></div>
-        <div class="plank plank-8"><img src="images/ad_logos/citi.png" class="traveler-logo"/></div>
-        <div class="plank plank-9"><img src="images/ad_logos/cheveron.png" class="traveler-logo"/></div>
-        <div class="plank plank-10"><img src="images/ad_logos/dell.png" class="traveler-logo"/></div>
-        <div class="plank plank-11"><img src="images/ad_logos/fedex.png" class="traveler-logo"/></div>
-        <div class="plank plank-12"><img src="images/ad_logos/citi.png" class="traveler-logo"/></div>
-        <div class="plank plank-13"><img src="images/ad_logos/cheveron.png" class="traveler-logo"/></div>
-        <div class="plank plank-14"><img src="images/ad_logos/fedex.png" class="traveler-logo"/></div>
-        <div class="plank plank-15"><img src="images/ad_logos/dell.png" class="traveler-logo"/></div>
-        <div class="plank plank-16"><img src="images/ad_logos/cheveron.png" class="traveler-logo"/></div>
-        <div class="plank plank-17"><img src="images/ad_logos/citi.png" class="traveler-logo"/></div>
-        <div class="plank plank-18"><img src="images/ad_logos/fedex.png" class="traveler-logo"/></div>
-        <div class="plank plank-19"><img src="images/ad_logos/cheveron.png" class="traveler-logo"/></div>
-        <div class="plank plank-20"><img src="images/ad_logos/dell.png" class="traveler-logo"/></div>
-        <div class="plank plank-21"><img src="images/ad_logos/citi.png" class="traveler-logo"/></div>
+        <div class="plank plank-1"><img src="{{ asset('images/ad_logos/fedex.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-2"><img src="{{ asset('images/ad_logos/citi.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-3"><img src="{{ asset('images/ad_logos/dell.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-4"><img src="{{ asset('images/ad_logos/cheveron.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-5"><img src="{{ asset('images/ad_logos/citi.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-6"><img src="{{ asset('images/ad_logos/dell.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-7"><img src="{{ asset('images/ad_logos/fedex.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-8"><img src="{{ asset('images/ad_logos/citi.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-9"><img src="{{ asset('images/ad_logos/cheveron.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-10"><img src="{{ asset('images/ad_logos/dell.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-11"><img src="{{ asset('images/ad_logos/fedex.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-12"><img src="{{ asset('images/ad_logos/citi.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-13"><img src="{{ asset('images/ad_logos/cheveron.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-14"><img src="{{ asset('images/ad_logos/fedex.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-15"><img src="{{ asset('images/ad_logos/dell.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-16"><img src="{{ asset('images/ad_logos/cheveron.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-17"><img src="{{ asset('images/ad_logos/citi.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-18"><img src="{{ asset('images/ad_logos/fedex.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-19"><img src="{{ asset('images/ad_logos/cheveron.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-20"><img src="{{ asset('images/ad_logos/dell.png') }}" class="traveler-logo"/></div>
+        <div class="plank plank-21"><img src="{{ asset('images/ad_logos/citi.png') }}" class="traveler-logo"/></div>
 
-        <div class="quote"><img src="{{ asset('images/uncompressed/quote.png') }}" alt=""/></div>
-        <div class="treasure"><img src="images/chest.png" alt=""/></div>
+        <div class="quote"><img src="{{ asset('images/quote.png') }}" alt=""/></div>
+        <div class="treasure"><img src="{{ asset('images/chest.png') }}" alt=""/></div>
     </div>
 </div>
 
